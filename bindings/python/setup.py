2cc8b4df09d5d8ab0be343f2cb8e00cad183723a
Stepan Zolotukhin <5089340+zolotukhin@users.noreply.github.com>
2025-12-04 22:42:29 -0800

    Moving to 0.1.1 with all the tests passing

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="hocdb",
-    version="0.1.0",
+    version="0.1.1",
     author="Heroes of Crypto AI",
     author_email="",
     description="High-Performance Time Series Database Python Bindings",
