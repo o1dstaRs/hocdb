163011196f0728b2280fa44ebe09ba8dc9eb59bf
zolotukhin <5089340+zolotukhin@users.noreply.github.com>
2026-01-04 15:53:29 -0800

    chore: cleanup temporary check files

--- check_io_file.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-
-pub fn main() !void {
-    if (@hasDecl(std.io, "File")) {
-        std.debug.print("std.io.File exists\n", .{});
-    } else {
-        std.debug.print("std.io.File does NOT exist locally\n", .{});
-    }
-}
